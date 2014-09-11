33b86ef74d46e35aa4d75b37c4e86abf12d0230e
Joel <joel@blanckien.local>
2014-09-11 14:22:53 -0700

    first couple exercises

---
+++ 1-1.php
@@ -0,0 +1,8 @@
+<html>
+<head><title>My first PHP script</title></head>
+<body>
+<p><?php echo "Hello world! ";
+	 	echo "Here's a tag with several echos"; ?>
+ 	</p>
+</body>
+</html>
---
+++ 1-2.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"    
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
+ <html xmlns="http://www.w3.org/1999/xhtml"  xml:lang="en" lang="en">
+ <head>
+ <meta http-equiv="content-type" content="text/html;charset=iso-8859-1" /> 
+ <title>My second PHP script</title>
+ </head>
+ <body>
+ <?php echo "Twinkle, twinkle little star<br/>";
+ 		$twinkle = "Twinkle";
+ 		$twinkler = "star";
+ 		echo "$twinkle, $twinkle, little $twinkler<br/>";
+ 		$twinkle = "Shine";
+ 		$twinkler = "crazy diamond";
+ 		echo "$twinkle, $twinkle little $twinkler"; ?>
+ </body>
+ </html>
