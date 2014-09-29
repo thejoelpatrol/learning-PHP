0a42964295d48c181dae6ebe4e45f0658f542316
thejoelpatrol <jcretan@gmail.com>
2014-09-29 11:51:38 -0700

    reorganize directories

--- 1-1.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head><title>My first PHP script</title></head>
-<body>
-<p><?php echo "Hello world! ";
-	 	echo "Here's a tag with several echos"; ?>
- 	</p>
-</body>
-</html>
--- 3-1b.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	
-	if (!empty($_POST['city']))
-		echo "Your favorite city is ".$_POST['city']."<br/>";	
-
-?>
